34c4f6a2f269803fa18e718439e0de5886b5e8dc
Adil <mr.adil777@gmail.com>
2023-12-19 23:07:33 +0600

    feat: add 1914 a, b

---
+++ cf/contest/1914/b/b.cpp
@@ -0,0 +1,48 @@
+// Bismillahir Rahmanir Rahim
+// chrono::system_clock::now().time_since_epoch().count()
+#include <bits/stdc++.h>
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define debug(x) cerr << #x << " = " << x << endl
+#define t true;
+#define f false;
+#define w(x) while (x)
+#define cend cout << endl
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+bool mcases = t;
+void yes() { cout << "YES" << endl; }
+void no() { cout << "NO" << endl; }
+
+void solve() {
+  int n, k;
+  cin >> n >> k;
+
+  for (int i = n - k; i >= 1; i--)
+    cout << i << ' ';
+  rep(i, n - k + 1, n + 1) cout << i << ' ';
+  cend;
+}
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tt = 1;
+  if (mcases)
+    cin >> tt;
+
+  while (tt--) {
+    solve();
+  }
+  return 0;
+}
