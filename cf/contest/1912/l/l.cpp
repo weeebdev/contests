3aad2e9406a95c229cb3f4a2e3f43b7d554a98f1
Adil <mr.adil777@gmail.com>
2023-12-21 22:51:56 +0600

    feat: add 1912 l

---
+++ cf/contest/1912/l/l.cpp
@@ -0,0 +1,66 @@
+// Bismillahir Rahmanir Rahim
+// chrono::system_clock::now().time_since_epoch().count()
+#include <bits/stdc++.h>
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define debug(x) cerr << #x << " = " << x << endl
+#define t true;
+#define f false;
+#define w(x) while (x)
+#define cend cout << endl
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+bool mcases = f;
+void yes() { cout << "YES" << endl; }
+void no() { cout << "NO" << endl; }
+
+void solve() {
+  int n;
+  string s;
+  cin >> n >> s;
+  map<char, int> m;
+  rep(i, 0, n) { m[s[i]]++; }
+
+  m['a'] = 0;
+  m['b'] = 0;
+
+  rep(i, 0, n - 1) {
+    m[s[i]]--;
+
+    if (s[i] == 'L') {
+      m['a']++;
+    } else {
+      m['b']++;
+    }
+
+    if (m['a'] != m['L'] && m['b'] != m['O']) {
+      cout << i + 1 << endl;
+      return;
+    }
+  }
+
+  cout << -1 << endl;
+}
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tt = 1;
+  if (mcases)
+    cin >> tt;
+
+  while (tt--) {
+    solve();
+  }
+  return 0;
+}
